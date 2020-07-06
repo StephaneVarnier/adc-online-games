a8d8fc48e0910b9222662be890d0e200d4ee6592
var.stephane <varnier.s@orange.fr>
2020-07-06 20:09:42 +0200

    giving up eureka config

--- src/main/java/archiduchess/microservice_onlinegame/MicroserviceOnlineGameApplication.java
+++ src/main/java/archiduchess/microservice_onlinegame/MicroserviceOnlineGameApplication.java
@@ -5,14 +5,13 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import chesspresso.pgn.PGNSyntaxError;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication (exclude = {DataSourceAutoConfiguration.class })
 @EnableSwagger2
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class MicroserviceOnlineGameApplication {
 
 	public static void main(String[] args) throws PGNSyntaxError, IOException {
