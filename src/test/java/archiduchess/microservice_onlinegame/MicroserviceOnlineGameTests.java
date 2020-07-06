042f325f3809ad43b08b5589e495fafc07de82df
var.stephane <varnier.s@orange.fr>
2020-07-06 17:21:23 +0200

    correcting test class and package names to solve build issue

--- src/test/java/archiduchess/microservice_onlinegame/MicroserviceOnlineGameTests.java
+++ src/test/java/archiduchess/microservice_onlinegame/MicroserviceOnlineGameTests.java
@@ -1,10 +1,10 @@
-package archiduchess.games;
+package archiduchess.microservice_onlinegame;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GamesApplicationTests {
+class MicroserviceOnlineGameTests {
 
 	@Test
 	void contextLoads() {
